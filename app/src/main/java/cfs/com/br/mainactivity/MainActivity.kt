3649818bca10e443c5ee4c37610cf29ac3473c92
fsclaudio <fsclaudio@gmail.com>
2022-07-05 11:00:53 -0300

    first Commit

---
+++ app/src/main/java/cfs/com/br/mainactivity/MainActivity.kt
@@ -0,0 +1,11 @@
+package cfs.com.br.mainactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
